500190715d8b48c2a45cabfd05fce71cc16d4f37
黃振寧 HUANG, CHENG-NING <92087079+HCN1222@users.noreply.github.com>
2023-08-17 23:40:45 +0800

    binary2bcd_div project update

---
+++ lab/lab3_combinational/binary2bcd_div/verilog/vivado/vivado.srcs/sources_1/new/binary2bcd.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company:
+// Engineer:
+//
+// Create Date: 08/17/2023 10:06:41 AM
+// Design Name:
+// Module Name: binary2bcd
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module binary2bcd(binary,
+                  bcd);
+    input [13:0] binary;
+    output [15:0] bcd;
+endmodule
